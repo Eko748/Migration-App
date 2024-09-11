ff2623e5613132ca5d1bdbce8376451cb8220583
DarCode <deomastar76@gmail.com>
2024-09-11 13:03:07 +0700

    create miggration table andalalin

---
+++ database/migrations/2024_09_11_060012_create_andalalin_trails_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('andalalin_trail', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('andalalin_id')->nullable();
+            $table->foreign('andalalin_id')->references('id')->on('andalalin')->onDelete('cascade');
+            $table->string('nama')->nullable();
+            $table->string('status')->nullable();
+            $table->text('detail')->nullable();
+            $table->timestamp('timestamp')->nullable();;
+            $table->string('updated_by')->nullable();
+            $table->string('created_by')->nullable();
+            $table->string('deleted_by')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('andalalin_trail');
+    }
+};
