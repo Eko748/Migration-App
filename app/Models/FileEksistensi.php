e268d506808054e422d2a37bb805422dd8220d7d
DarCode <deomastar76@gmail.com>
2024-09-11 13:49:37 +0700

    create miggration table file

---
+++ app/Models/FileEksistensi.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FileEksistensi extends Model
+{
+    use HasFactory;
+}
