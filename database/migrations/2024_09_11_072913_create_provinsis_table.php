0bbcae78f98710f4d774337c5cc0e799f742df55
Eko748 <permanaeko74@gmail.com>
2024-09-11 14:39:02 +0700

    perf(migration)

---
+++ database/migrations/2024_09_11_072913_create_provinsis_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('provinsi', function (Blueprint $table) {
+            $table->id();
+            $table->integer('kode')->nullable();
+            $table->string('nama')->nullable();
+            $table->integer('bptd_id')->nullable();
+            $table->string('created_by')->nullable();
+            $table->string('updated_by')->nullable();
+            $table->string('deleted_by')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('provinsi');
+    }
+};
