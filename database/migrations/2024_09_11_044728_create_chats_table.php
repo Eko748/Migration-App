a192f59d42d5cb1dcc9971e3c0db3705f8f45f7b
DarCode <deomastar76@gmail.com>
2024-09-11 14:23:18 +0700

    create migration 5 table

--- database/migrations/2024_09_11_044728_create_chats_table.php
+++ database/migrations/2024_09_11_044728_create_chats_table.php
@@ -27,7 +27,6 @@ return new class extends Migration
             $table->string('deleted_by')->nullable();
             $table->string('created_by')->nullable();
             $table->string('updated_by')->nullable();
-            $table->string('deleted_by')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
